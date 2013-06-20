38642065a7dea9e9b4cac51afaf493108caa654b
keoken <keokenppb@gmail.com>
2013-06-20 19:58:00 +0900

    serverspecのテスト内容更新

--- sample_app_manifest.pp
+++ sample_app_manifest.pp
@@ -1,1 +1,14 @@
-notice("Hello, World!")
+yumrepo { 'nginx':
+  describe => 'nginx yum repository',
+  baseurl => 'http://nginx.org/packages/centos/6/$basearch/',
+  enabled => 1,
+  gpgcheck => 0,
+}
+
+package { 'nginx':
+  ensure => installed,
+  require => Yumrepo['nginx'],
+}
+
+$port = 80
+
---
+++ spec/app002.keoken.pb/sample_app_spec.rb
@@ -0,0 +1,52 @@
+require 'spec_helper'
+
+describe group('appuser') do
+  it { should exist }
+  it { should have_gid 1000 }
+end
+
+
+
+describe user('appuser') do
+  it { should exist }
+  it { should have_uid 1000 }
+  it { should belong_to_group 'appuser' }
+  it { should have_home_directory '/home/appuser' }
+end
+
+describe file('/home/appuser/sample_app/tmp/sockets/.unicorn.sock') do
+  it { should be_socket }
+end
+
+describe package('nginx') do
+  it { should be_installed }
+end
+
+describe service('nginx') do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe port(80) do
+  it { should be_listening }
+end
+
+describe file('/etc/nginx/nginx.conf') do
+  it { should contain 'user nginx nginx;' }
+  it { should contain 'server unix:/home/appuser/sample_app/tmp/sockets/.unicorn.sock fail_timeout=0;' }
+  it { should contain('root /home/appuser/sample_app/public;').from(/^\s+server {/).to(/^\s+location @app/) }
+end
+
+describe package('mysql-server') do
+  it { should be_installed }
+end
+
+describe service('mysqld') do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe port(3306) do
+  it { should be_listening }
+end
+
