6444c5cbeb660c9391f038b324ee4b24537e8164
武尾 健太 <keoken@app002.keoken.pb>
2013-06-20 14:38:45 +0900

    パッケージのインストールとユーザの作成のためにmanifestfileを編集した

--- sample_app_manifest.pp
+++ sample_app_manifest.pp
@@ -1,1 +1,32 @@
-notice("Hello, World!")
+package { 
+  [
+    'zsh',
+    'emacs',
+    'nginx',
+    'mysql-server',
+    'mysql-devel',
+    'gcc',
+    'gcc-c++',
+    'make',
+    'openssl',
+    'openssl-devel',
+    'libxml2-devel',
+    'libxslt-devel',
+  ]:
+  ensure => installed,
+}
+
+user { 'appuser':
+  ensure => present,
+  uid => 1000,
+  gid => appuser,
+  comment => 'appuser',
+  home => '/home/appuser',
+  managehome => ture,
+  shell => '/bin/bash'
+}
+
+group { 'appuser':
+  ensure => present,
+  gid => 1000,
+}
