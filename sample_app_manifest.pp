bf3c5f342f7863c31d91e37114a2536d73145ec5
武尾 健太 <keoken@app002.keoken.pb>
2013-06-21 17:53:34 +0900

    rbenvとrubyインストール、MySQLのユーザ作成のmanifestを作成した

--- sample_app_manifest.pp
+++ sample_app_manifest.pp
@@ -1,4 +1,4 @@
-package { 
+package {
   [
     'zsh',
     'emacs',
@@ -12,6 +12,7 @@ package {
     'openssl-devel',
     'libxml2-devel',
     'libxslt-devel',
+    'git',
   ]:
   ensure => installed,
 }
@@ -47,7 +48,7 @@ file { '/etc/nginx/nginx.conf':
   notify => Service['nginx'],
 }
 
-file { '/etc/my.conf':
+file { '/etc/my.conf':,
   content => "character-set-server = utf8",
 }
 
@@ -57,3 +58,67 @@ service { 'mysqld':
   hasrestart => true,
   require => Package['mysql-server'],
 }
+
+Exec { 
+  path => '/usr/bin:/bin:/usr/local/bin',
+}
+
+exec { 'rbenv':
+  cwd => '/usr/local',
+  user => 'root',
+  command => 'git clone git://github.com/sstephenson/rbenv.git;chgrp -R appuser rbenv;chmod -R g+rwxXs rbenv;mkdir /usr/local/rbenv/plugins',
+  creates => '/usr/local/rbenv',  
+  require => Package['git'],
+}
+
+exec { 'path-rbenv':
+  command => "echo 'export RBENV_ROOT=\"/usr/local/rbenv\"' >> /etc/profile.d/rbenv.sh;
+  	      echo 'export PATH=\"/usr/local/rbenv/bin:\$PATH\"' >> /etc/profile.d/rbenv.sh;
+              echo 'eval \"$(rbenv init -)\"' >> /etc/profile.d/rbenv.sh",
+  unless => 'test -f /etc/profile.d/rbenv.sh',
+  require => Exec['rbenv'],
+}
+
+#exec { 'use-rbenv-command':
+#  command => 'sh -c "source /etc/profile.d/rbenv.sh"',
+#  require => Exec['path-rbenv'],
+#}
+
+exec { 'ruby-build':
+  cwd => '/usr/local/rbenv/plugins',
+  user => 'root',
+  command => 'git clone git://github.com/sstephenson/ruby-build.git;cd ruby-build;./install.sh;chgrp -R appuser ruby-build;chmod -R g+rwxs ruby-build',
+  creates => '/usr/local/rbenv/plugins/ruby-build',
+  require => Exec['rbenv'],
+}
+
+exec { 'ruby-2.0.0p195-make-directory':
+  cwd => '/usr/local/rbenv',
+  user => 'root',
+  command => 'mkdir -p /usr/local/rbenv/versions/2.0.0-p195',
+  creates => '/usr/local/rbenv/versions/2.0.0-p195',
+  require => Exec[rbenv],
+}
+
+exec { 'ruby2.0.0p195-install':
+  cwd => '/usr/local/rbenv/versions/2.0.0-p195',
+  user => 'root',
+  command => 'ruby-build 2.0.0-p195 /usr/local/rbenv/versions/2.0.0-p195',
+  creates => '/usr/local/rbenv/versions/2.0.0-p195/bin',
+  timeout => 0,
+  require => Exec['ruby-2.0.0p195-make-directory'],
+}
+
+exec { 'use-ruby2.0.0p195':
+  command => 'echo "2.0.0-p195" > /usr/local/rbenv/version',
+#  command => 'sh -c "source /etc/profile.d/rbenv.sh;rbenv global 2.0.0-p195"',
+  require => Exec['ruby2.0.0p195-install'],
+}
+
+exec { 'mysql-create-user':
+ command => 'mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO keoken@localhost IDENTIFIED BY \'passwd\'"',
+ unless => 'mysql -u root -e "select User , Host from mysql.user where User = \'keoken\' and Host = \'localhost\'"',
+}
+
+
+
