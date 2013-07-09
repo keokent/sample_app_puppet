89dfe6c2c69f487d17a8b7337283a609464c71ca
keoken <keokenppb@gmail.com>
2013-07-09 19:41:04 +0900

    mysqlのユーザをlocalhostからapp001に変更

--- roles/db/manifests/setup_mysql.pp
+++ roles/db/manifests/setup_mysql.pp
@@ -1,8 +1,8 @@
 class db::setup_mysql {
   exec { 'mysql-create-user':
     path => '/usr/bin:/bin',
-    command => 'mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO keoken@localhost IDENTIFIED BY \'passwd\'"',
-    unless => 'mysql -u root -e "select User from mysql.user where User = \'keoken\' and Host = \'localhost\'\G" | grep keoken',
+    command => 'mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO keoken@'192.168.48.*' IDENTIFIED BY \'passwd\'"',
+    unless => 'mysql -u root -e "select User from mysql.user where User = \'keoken\' and Host = \'192.168.48.*\'\G" | grep keoken',
   }
 
   exec { 'mysql-create-database':
