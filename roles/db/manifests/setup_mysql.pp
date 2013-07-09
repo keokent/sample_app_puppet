d6d2bde07b44a195e1d6bc2c74c59c2fbafd78b5
keoken <keokenppb@gmail.com>
2013-07-09 19:46:57 +0900

    fix typo

--- roles/db/manifests/setup_mysql.pp
+++ roles/db/manifests/setup_mysql.pp
@@ -1,7 +1,7 @@
 class db::setup_mysql {
   exec { 'mysql-create-user':
     path => '/usr/bin:/bin',
-    command => 'mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO keoken@'192.168.48.*' IDENTIFIED BY \'passwd\'"',
+    command => 'mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO keoken@\'192.168.48.*\' IDENTIFIED BY \'passwd\'"',
     unless => 'mysql -u root -e "select User from mysql.user where User = \'keoken\' and Host = \'192.168.48.*\'\G" | grep keoken',
   }
 
