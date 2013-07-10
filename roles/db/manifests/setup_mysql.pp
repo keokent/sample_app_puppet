7fb04ba1167c3b3fe3523fe89a82f8d1cb79b232
keoken <keokenppb@gmail.com>
2013-07-10 16:21:50 +0900

    mysqlユーザのホストをIPアドレスで指定

--- roles/db/manifests/setup_mysql.pp
+++ roles/db/manifests/setup_mysql.pp
@@ -1,8 +1,8 @@
 class db::setup_mysql {
   exec { 'mysql-create-user':
     path => '/usr/bin:/bin',
-    command => 'mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO keoken@app001.keoken.pblan IDENTIFIED BY \'passwd\'"',
-    unless => 'mysql -u root -e "select User from mysql.user where User = \'keoken\' and Host = \'app001.keoken.pblan\'\G" | grep keoken',
+    command => 'mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO keoken@\'192.168.48.%\' IDENTIFIED BY \'passwd\'"',
+    unless => 'mysql -u root -e "select User from mysql.user where User = \'keoken\' and Host = \'192.168.48.%\'\G" | grep keoken',
   }
 
   exec { 'mysql-create-database':
