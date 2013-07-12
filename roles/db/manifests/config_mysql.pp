c87e7f7027f7cff210f37764bde161ed0119ff1e
keoken <keokenppb@gmail.com>
2013-07-12 12:44:17 +0900

    dbロールのミスを修正

--- roles/db/manifests/config_mysql.pp
+++ roles/db/manifests/config_mysql.pp
@@ -1,8 +1,8 @@
-class mysql::config {
+class db::config_mysql {
   file { '/etc/my.cnf':
     owner => 'root',
     group => 'root',
-    mode => '0644'
-    content => template['my.cnf'],
+    mode => '0644',
+    content => template['db/my.cnf'],
   }
 }
--- spec/app002.keoken.pb/sample_app_spec.rb
+++ spec/app002.keoken.pb/sample_app_spec.rb
@@ -58,7 +58,7 @@ end
 
 describe package('memcached') do
   it { should be_installed }
-}
+end
 
 describe port(11211) do
   it { should be_listening }
